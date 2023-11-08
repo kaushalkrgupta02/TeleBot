0d03d0afefb05cf0ac4238177e099953b651e365
Kaushal Gupta <114843073+kaushalkrgupta02@users.noreply.github.com>
2023-11-09 01:15:55 +0530

    Update link_shortner_tele_bot_command.py

--- link_shortner_tele_bot_command.py
+++ link_shortner_tele_bot_command.py
@@ -1,33 +1,37 @@
 import json
 import requests
 
+token_no = ""
 
+def sendMessage(id, msg):
+  url = f'https://api.telegram.org/bot{token_no}/sendMessage'
+  payload = {
+    'chat_id': id,
+    'text': msg
+  }
+  r  = requests.post(url, json=payload)
+  
+def isValidUrl(msg):
+  if(msg[:4]=="http" or msg[:3]=="www"):
+    return True
+  return False
+      
 def lambda_handler(event, context):
-    print(event)
-    try:
-        body=json.loads(event['body'])
-        print(body)
+  try:
+    body = json.loads(event['body'])
+    message_part = body['message'].get('text')
+    chat_id = body['message']['chat']['id']
 
-        message_part=body['message'].get('text')
-        print("Message part : {}".format(message_part))
-
-        data = {'url':message_part}
-
-        payload=requests.post('https://cleanuri.com/api/v1/shorten',data=data) 
-        short_url = payload.json()['result_url']
-        print("the short url is : {}".format(short_url))
-
-        chat_id=body['message']['chat']['id']
-        url = f'https://api.telegram.org/bot{token_no}/sendMessage'
-        payload={
-                  'chat_id': chat_id,
-                  'text':short_url
-                }
-        r = requests.post(url,json=payload)
-        return {
-               "statusCode": 200
-        }
-    except:
-        return {
-               "statusCode": 200
-        }
+    if isValidUrl(message_part):
+      short_url = requests.post(f'https://api.shrtco.de/v2/shorten?url={message_part}').json()['result']['full_short_link']
+      sendMessage(chat_id, short_url)
+      return {
+        "statusCode": 200
+      }
+      
+    else:
+      sendMessage(chat_id,"Hello,"+"\n"+"Please Enter a Valid URL")
+  except:
+    return {
+      "statusCode": 500
+    }
